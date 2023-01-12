eab82d75d57055df8036f90aabaa7f1a39f6668b
alariojo99 <104770022+alariojo99@users.noreply.github.com>
2023-01-12 19:27:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,69 +5,49 @@ const cakeBtn = document.getElementById('cake-btn')
 const candleBtn = document.getElementById('candle-btn')
 const lighterBtn = document.getElementById('lighter-btn')
 
-//Pastel
-function cake(){
-    container.addEventListener('click', cakeHandler)
-    cakeBtn.removeEventListener('click', cake)
-}
+const menu = document.getElementsByClassName('header-btns')
 
-function cakeHandler(e){
-    if(e.target.matches('button') === false){
+let buttonClicked = ''
+
+cakeBtn.addEventListener('click', function(){
+    buttonClicked = 'cakeBtn'
+})
+
+candleBtn.addEventListener('click', function(){
+    buttonClicked = 'candleBtn'
+})
+
+lighterBtn.addEventListener('click', function(){
+    buttonClicked = 'lighterBtn'
+})
+
+container.addEventListener('click', function(e){
+    console.log(e.target)
+    if ((buttonClicked === 'cakeBtn')&&(e.target.matches('button')===false)){
         const cake = document.createElement('img')
-        cake.src = 'img/chocolatecake_nb.png'
-        cake.setAttribute('class','cake')
+        cake.src='img/chocolatecake_nb.png'
+        cake.setAttribute('class', 'cake')
         cake.style.left = (e.x-350) + 'px'
         cake.style.top = (e.y-680) + 'px'
 
         container.append(cake)
-        container.removeEventListener('click', cakeHandler)
+    }else if ((buttonClicked === 'candleBtn')&&(e.target.matches('button')===false)){
+        const candle = document.createElement('img')
+        candle.src = 'img/candle2.png'
+        candle.setAttribute('class','candle')
+        candle.style.left = (e.x-14) + 'px'
+        candle.style.top = (e.y-112) + 'px'
+
+        container.append(candle)
+    }else if ((buttonClicked === 'lighterBtn')&&(e.target.matches('button')===false)){
+        const light = document.createElement('img')
+        light.src = 'img/flame.png'
+        light.setAttribute('class','light')
+        light.style.left = (e.x-15) + 'px'
+        light.style.top = (e.y-50) + 'px'
+
+        container.append(light)
     }
-}
-
-//Velas
-let numberOfCandles = 0;
-candleBtn.addEventListener('click', function(){
-    container.addEventListener('click', function(e){
-        if(numberOfCandles === 24){
-            container.removeEventListener('click', arguments.callee);
-            return;
-        }
-
-        if(e.target.matches('button') === false){
-            const candle = document.createElement('img')
-            candle.src = 'img/candle.png'
-            candle.setAttribute('class','candle')
-            candle.style.left = (e.x-14) + 'px'
-            candle.style.top = (e.y-112) + 'px'
-    
-            container.append(candle)
-            numberOfCandles++;
-    
-        }
-    })
-})
-
-//Llamas
-let numberOfLights = 0;
-lighterBtn.addEventListener('click', function(){
-    container.addEventListener('click', function(e){
-        if(numberOfLights === 24){
-            container.removeEventListener('click', arguments.callee);
-            return;
-        }
-
-        if(e.target.matches('button') === false){
-            const light = document.createElement('img')
-            light.src = 'img/flame.png'
-            light.setAttribute('class','light')
-            light.style.left = (e.x-15) + 'px'
-            light.style.top = (e.y-50) + 'px'
-    
-            container.append(light)
-            numberOfLights++;
-    
-        }
-    })
 })
 
 //Apagar velas
